eb1e62dd40ecfc57e1c8f8ed475fde12594a7078
Tomas Kramar <kramar.tomas@gmail.com>
2009-11-18 21:27:07 +0100

    CSVWriter deletes quotes

--- handlers.py
+++ handlers.py
@@ -4,6 +4,11 @@ class CSVWriter:
 
   def page(self, page, topic):
     if page != None and page != "" and topic != None and topic != "":
+      page = page.replace('"', '')
+      page = page.replace('&quot;', '')
+      topic = topic.replace('"', '')
+      topic = topic.replace('&quot;', '')
+
       self._file.write('"%(page)s","%(topic)s"\n' % {'page': page, 'topic': topic})
     else:
       print "Skipping page, one of the attributes is missing"
