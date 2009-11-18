faad6566d6a18c78e92a8b7e706565b809d2e4d8
Tomas Kramar <kramar.tomas@gmail.com>
2009-11-18 20:45:11 +0100

    Fixes CSV format
    Using , instead of ;
    No space between entries
    Line is written only when both attributes are present

--- handlers.py
+++ handlers.py
@@ -3,7 +3,10 @@ class CSVWriter:
     self._file = open(name, 'w')
 
   def page(self, page, topic):
-    self._file.write('%(page)s; %(topic)s\n' % {'page': page, 'topic': topic})
+    if page != None and page != "" and topic != None and topic != "":
+      self._file.write('"%(page)s","%(topic)s"\n' % {'page': page, 'topic': topic})
+    else:
+      print "Skipping page, one of the attributes is missing"
 
   def finish(self):
     self._file.close()
