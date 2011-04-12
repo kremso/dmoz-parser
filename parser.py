c6389d1b351005decbd4bdbd4f1f9bc1fdb0f63c
Joseph Turian <turian@gmail.com>
2011-04-12 21:39:23 +0000

    Strictly assert that 'topic' is at the end of every 'ExternalPage'

--- parser.py
+++ parser.py
@@ -29,6 +29,7 @@ class DmozHandler(handler.ContentHandler):
     self._current_page = ''
     self._capture_content = False
     self._current_content = {}
+    self._expect_end = False
 
   def startElement(self, name, attrs):
     if name == 'ExternalPage':
@@ -38,8 +39,16 @@ class DmozHandler(handler.ContentHandler):
       self._capture_content = True
       self._capture_content_type = name
 
+  def endElement(self, name):
+    # Make sure that the only thing after "topic" is "/ExternalPage"
+    if self._expect_end:
+      assert name == 'topic' or name == 'ExternalPage'
+      if name == 'ExternalPage':
+          self._expect_end = False
+
   def characters(self, content):
     if self._capture_content:
+      assert not self._expect_end
       self._current_content[self._capture_content_type] = content
 #      print self._capture_content_type, self._current_content[self._capture_content_type]
       if self._capture_content_type == "topic":
@@ -51,6 +60,7 @@ class DmozHandler(handler.ContentHandler):
         #     <topic>Top/Arts/Animation</topic>
         #   </ExternalPage>
           self._handler.page(self._current_page, self._current_content)
+          self._expect_end = True
       self._capture_content = False
 
   def endDocument(self):
