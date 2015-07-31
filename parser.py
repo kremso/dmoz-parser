803875ad79988e6a62b4dbd6a8ce3706c0b3b0cb
Zikes, Jan <jan.zikes@merck.com>
2015-07-31 17:03:21 +0200

    bugfixes

--- parser.py
+++ parser.py
@@ -103,7 +103,8 @@ if __name__ == '__main__':
     elif file_extension == ".csv":
         parser.add_handler(CSVWriter(output_path))
     else:
-        logger.info("Currently are only supported output files with extension .json or .scv")
+        logger.info("Currently are only supported output files with extension .json or .csv")
+        sys.exit(1)
     parser.run()
 
     logger.info("finished running %s", program)
